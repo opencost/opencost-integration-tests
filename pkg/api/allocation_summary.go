bc5d06c4caa3d58f4db0f78939d3f6b8f4d8ca1f
Manas Sivakumar <manas23601@gmail.com>
2025-06-17 22:43:30 +0530

    did gofmt

--- pkg/api/allocation_summary.go
+++ pkg/api/allocation_summary.go
@@ -2,26 +2,26 @@ package api
 
 // GetAllocation requests GET /allocation
 func (api *API) GetAllocationSummary(req AllocationRequest) (*AllocationSummaryResponse, error) {
-	resp := &AllocationSummaryResponse{}
+        resp := &AllocationSummaryResponse{}
 
-	err := api.GET("/allocation/summary", req, resp)
-	if err != nil {
-		return nil, err
-	}
-	return resp, nil
+        err := api.GET("/allocation/summary", req, resp)
+        if err != nil {
+                return nil, err
+        }
+        return resp, nil
 }
 
 type AllocationSummaryResponse struct {
-	Code int								`json:"code"`
-	Data AllocationSummaryData				`json:"data"`
+        Code int                   `json:"code"`
+        Data AllocationSummaryData `json:"data"`
 }
 
 type AllocationSummaryData struct {
-	Step int								`json:"step"`
-	Sets []AllocationSummaryDataItem               `json:"sets`											
+        Step int                         `json:"step"`
+        Sets []AllocationSummaryDataItem `json:"sets`
 }
 
 type AllocationSummaryDataItem struct {
-	Allocations map[string]AllocationResponseItem		`json:"allocations"`
-	Window Window										`json:"window`		
+        Allocations map[string]AllocationResponseItem `json:"allocations"`
+        Window      Window                            `json:"window`
 }
--- test/integration/query/count/allocations_summary_test.go
+++ test/integration/query/count/allocations_summary_test.go
@@ -4,72 +4,71 @@ package count
 // and allocation/summary API request
 
 import (
-	// "fmt"
-	// "time"
-	"testing"
-	"github.com/opencost/opencost-integration-tests/pkg/api"
-	"github.com/opencost/opencost-integration-tests/pkg/prometheus"
+        // "fmt"
+        // "time"
+        "github.com/opencost/opencost-integration-tests/pkg/api"
+        "github.com/opencost/opencost-integration-tests/pkg/prometheus"
+        "testing"
 )
 
 func TestQueryAllocationSummary(t *testing.T) {
-	apiObj := api.NewAPI()
+        apiObj := api.NewAPI()
 
-	testCases := []struct {
-		name		string
-		window      string
-		aggregate   string
-		accumulate  string
-	}{
-		{
-			name: "Yesterday",
-			window: "24h",
-			aggregate: "pod",
-			accumulate: "false",
-		},
-	}
+        testCases := []struct {
+                name       string
+                window     string
+                aggregate  string
+                accumulate string
+        }{
+                {
+                        name:       "Yesterday",
+                        window:     "24h",
+                        aggregate:  "pod",
+                        accumulate: "false",
+                },
+        }
 
-	t.Logf("testCases: %v", testCases)
+        t.Logf("testCases: %v", testCases)
 
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
+        for _, tc := range testCases {
+                t.Run(tc.name, func(t *testing.T) {
 
-			// API Client
-			apiResponse, err := apiObj.GetAllocationSummary(api.AllocationRequest{
-				Window: tc.window,
-				Aggregate: tc.aggregate,
-				Accumulate: tc.accumulate,
-			})
+                        // API Client
+                        apiResponse, err := apiObj.GetAllocationSummary(api.AllocationRequest{
+                                Window:     tc.window,
+                                Aggregate:  tc.aggregate,
+                                Accumulate: tc.accumulate,
+                        })
 
-			if err != nil {
-				t.Fatalf("Error while calling Allocation API %v", err)
-			}
-			if apiResponse.Code != 200 {
-				t.Errorf("API returned non-200 code")
-			}
+                        if err != nil {
+                                t.Fatalf("Error while calling Allocation API %v", err)
+                        }
+                        if apiResponse.Code != 200 {
+                                t.Errorf("API returned non-200 code")
+                        }
 
-			// Prometheus Client
-			client := prometheus.NewClient()
-			metric := "kube_pod_container_status_running"
-			// kube-state-metrics is another job type
-			filters := map[string]string{
-				"job": "opencost",
-			}
-			promResponse, err := client.RunPromQLQuery(metric, filters, tc.window)
+                        // Prometheus Client
+                        client := prometheus.NewClient()
+                        metric := "kube_pod_container_status_running"
+                        // kube-state-metrics is another job type
+                        filters := map[string]string{
+                                "job": "opencost",
+                        }
+                        promResponse, err := client.RunPromQLQuery(metric, filters, tc.window)
 
-			if err != nil {
-				t.Fatalf("Error while calling Prometheus API %v", err)
-			}
-			
-			apiAllocationsSummaryCount := len(apiResponse.Data.Sets[0].Allocations)
-			promAllocationsSummaryCount := len(promResponse.Data.Result)
+                        if err != nil {
+                                t.Fatalf("Error while calling Prometheus API %v", err)
+                        }
 
-			if apiAllocationsSummaryCount != promAllocationsSummaryCount {
-				t.Errorf("Number of Allocations Responses from Prometheus %d and /allocation/summary %d did not match.", promAllocationsSummaryCount, apiAllocationsSummaryCount)
-			} else {
-				t.Logf("Number of Allocations from Promtheus and /allocation/summary match")
-			}
+                        apiAllocationsSummaryCount := len(apiResponse.Data.Sets[0].Allocations)
+                        promAllocationsSummaryCount := len(promResponse.Data.Result)
 
+                        if apiAllocationsSummaryCount != promAllocationsSummaryCount {
+                                t.Errorf("Number of Allocations Responses from Prometheus %d and /allocation/summary %d did not match.", promAllocationsSummaryCount, apiAllocationsSummaryCount)
+                        } else {
+                                t.Logf("Number of Allocations from Promtheus and /allocation/summary match")
+                        }
 
-		})
-	}
+                })
+        }
 }
