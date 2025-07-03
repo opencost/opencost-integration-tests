378c4f32df95f6cc98862afa10a682232779f408
Manas Sivakumar <manas23601@gmail.com>
2025-07-03 19:03:08 +0530

    RAM Max Usage Costs

--- test/integration/prometheus/ram_maxtime_ground_truth_test.go
+++ test/integration/prometheus/ram_maxtime_ground_truth_test.go
@@ -2,28 +2,68 @@ package prometheus
 
 // Description - Checks for Ram Byte Hours from prometheus and /allocation are the same
 
+// RawAllocationOnlyData is information that only belong in "raw" Allocations,
+// those which have not undergone aggregation, accumulation, or any other form
+// of combination to produce a new Allocation from other Allocations.
+//
+// Max usage data belongs here because computing the overall maximum from two
+// or more Allocations is a non-trivial operation that cannot be defined without
+// maintaining a large amount of state. Consider the following example:
+// _______________________________________________
+//
+// A1 Using 3 CPU    ----      -----     ------
+// A2 Using 2 CPU      ----      -----      ----
+// A3 Using 1 CPU         ---       --
+// _______________________________________________
+//
+//	Time ---->
+//
+// The logical maximum CPU usage is 5, but this cannot be calculated iteratively,
+// which is how we calculate aggregations and accumulations of Allocations currently.
+// This becomes a problem I could call "maximum sum of overlapping intervals" and is
+// essentially a variant of an interval scheduling algorithm.
+//
+// If we had types to differentiate between regular Allocations and AggregatedAllocations
+// then this type would be unnecessary and its fields would go into the regular Allocation
+// and not in the AggregatedAllocation.
+
 import (
 	// "fmt"
 	// "time"
-	"testing"
-	"strconv"
 	"github.com/opencost/opencost-integration-tests/pkg/api"
 	"github.com/opencost/opencost-integration-tests/pkg/prometheus"
+	"math"
+	"testing"
 )
+
+const tolerance = 0.05
+
+func AreWithinPercentage(num1, num2, tolerance float64) bool {
+	// Checks if two numbers are within a certain absolute range of each other.
+	if num1 == 0 && num2 == 0 {
+		return true
+	}
+
+	tolerance = math.Abs(tolerance)
+	diff := math.Abs(num1 - num2)
+	reference := math.Max(math.Abs(num1), math.Abs(num2))
+
+	return diff <= (reference * tolerance)
+}
 
 func TestRAMMax(t *testing.T) {
 	apiObj := api.NewAPI()
 
 	testCases := []struct {
-		name		string
-		window      string
-		aggregate   string
-		accumulate  string
+		name       string
+		window     string
+		aggregate  string
+		accumulate string
 	}{
 		{
-			name: "Yesterday",
-			window: "24h",
-			aggregate: "namespace",
+			name:       "Yesterday",
+			window:     "24h",
+			aggregate:  "pod",
 			accumulate: "false",
 		},
 	}
@@ -33,10 +73,65 @@ func TestRAMMax(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 
+			// Any data that is in a "raw allocation only" is not valid in any
+			// sort of cumulative Allocation (like one that is added).
+
+			type RAMUsageMaxAggregate struct {
+				AllocationUsageMax float64
+				PrometheusUsageMax float64
+				Namespace          string
+			}
+			ramUsageMaxPodMap := make(map[string]*RAMUsageMaxAggregate)
+
+			////////////////////////////////////////////////////////////////////////////
+			// RAMMaxUsage Calculation
+
+			// max(max_over_time(container_memory_working_set_bytes{
+			//     container!="", container_name!="POD", container!="POD", %s}[%s]))
+			// by
+			// (container_name, container, pod_name, pod, namespace, node, instance, %s)
+			////////////////////////////////////////////////////////////////////////////
+
+			// Collect Namespace results from Prometheus
+			client := prometheus.NewClient()
+			promInput := prometheus.PrometheusInput{
+				Metric: "container_memory_working_set_bytes",
+			}
+			ignoreFilters := map[string][]string{
+				"container": {"", "POD"},
+				"node":      {""},
+			}
+			promInput.Function = []string{"max_over_time", "max"}
+			promInput.QueryWindow = tc.window
+			promInput.IgnoreFilters = ignoreFilters
+			promInput.AggregateBy = []string{"container", "pod", "namespace", "node", "instance"}
+			promResponse, err := client.RunPromQLQuery(promInput)
+			// Do we need container_name and pod_name
+			if err != nil {
+				t.Fatalf("Error while calling Prometheus API %v", err)
+			}
+
+			for _, promResponseItem := range promResponse.Data.Result {
+				ramUsageMaxPod, ok := ramUsageMaxPodMap[promResponseItem.Metric.Pod]
+				if !ok {
+					ramUsageMaxPodMap[promResponseItem.Metric.Pod] = &RAMUsageMaxAggregate{
+						PrometheusUsageMax: promResponseItem.Value.Value,
+						AllocationUsageMax: 0.0,
+						Namespace:          promResponseItem.Metric.Namespace,
+					}
+					continue
+				}
+				ramUsageMaxPod.PrometheusUsageMax = max(ramUsageMaxPod.PrometheusUsageMax, promResponseItem.Value.Value)
+			}
+
+			/////////////////////////////////////////////
 			// API Client
+			/////////////////////////////////////////////
+
+			// Why doesn't allocation work on Namespace aggregate?
 			apiResponse, err := apiObj.GetAllocation(api.AllocationRequest{
-				Window: tc.window,
-				Aggregate: tc.aggregate,
+				Window:     tc.window,
+				Aggregate:  tc.aggregate,
 				Accumulate: tc.accumulate,
 			})
 
@@ -46,51 +141,51 @@ func TestRAMMax(t *testing.T) {
 			if apiResponse.Code != 200 {
 				t.Errorf("API returned non-200 code")
 			}
-			// Prometheus Client
-			client := prometheus.NewClient()
-			promInput := prometheus.PrometheusInput{
-				Metric: "container_memory_allocation_bytes",
-			}
-			//Get CPU Core hours from each namespace
-			for namespace, allocationResponseItem := range apiResponse.Data[0] {
 
-				////////////////////////////////////////////////////////////////////////////
-				// RAMByteAllocated Calculation
-				promNamespaceRAMByteMaxUsage := 0.0
-				// Collect Namespace results from Prometheus
-				filters := map[string]string{
-					"job": "opencost",
-					"namespace": namespace,
-				}
-				ignoreFilters := map[string][]string{
-					"container": {"", "POD"},
-					"node": {""},
-				}
-				promInput.Filters = filters
-				promInput.Function = "max_over_time"
-				promInput.Window = tc.window
-				promInput.IgnoreFilters = ignoreFilters
-				promResponse, err := client.RunPromQLQuery(promInput)
-				if err != nil {
-					t.Fatalf("Error while calling Prometheus API %v", err)
-				}
-				for _, promResponseItem := range promResponse.Data.Result {
-					valueStr, _ := promResponseItem.Value[1].(string)
-					floatVal, err := strconv.ParseFloat(valueStr, 64)
-					if err != nil {
-						t.Errorf("Error parsing metric value '%s' to float64 for pod %s, namespace %s: %v",
-							valueStr, promResponseItem.Metric.Pod, namespace, err)
-						// Decide how to handle this error (e.g., continue, log, set to 0)
-					} else {
-						promNamespaceRAMByteMaxUsage += floatVal
+			for _, allocationResponseItem := range apiResponse.Data[0] {
+				ramUsageMaxPod, ok := ramUsageMaxPodMap[allocationResponseItem.Properties.Pod]
+				if !ok {
+					ramUsageMaxPodMap[allocationResponseItem.Properties.Pod] = &RAMUsageMaxAggregate{
+						AllocationUsageMax: allocationResponseItem.RawAllocationsOnly.RAMByteUsageMax,
 					}
+					continue
 				}
+				ramUsageMaxPod.AllocationUsageMax = max(ramUsageMaxPod.AllocationUsageMax, allocationResponseItem.RawAllocationsOnly.RAMByteUsageMax)
+			}
 
-			
-				if promNamespaceRAMByteMaxUsage != allocationResponseItem.RawAllocationsOnly.RAMByteUsageMax {
-					t.Errorf("RAM Byte Hours Sum does not match for prometheus %f and /allocation %f for namespace %s", promNamespaceRAMByteMaxUsage, allocationResponseItem.RawAllocationsOnly.RAMByteUsageMax, namespace)	
+			// Windows are not accurate for prometheus and allocation
+			for pod, ramMaxUsageValues := range ramUsageMaxPodMap {
+				t.Logf("Pod %s", pod)
+				if !AreWithinPercentage(ramMaxUsageValues.PrometheusUsageMax, ramMaxUsageValues.AllocationUsageMax, tolerance) {
+					t.Errorf("RAMUsageMax[Fail]: Prometheus: %0.2f, /allocation: %0.2f", ramMaxUsageValues.PrometheusUsageMax, ramMaxUsageValues.AllocationUsageMax)
 				} else {
-					t.Logf("RAM Byte Hours Match for namespace %s", namespace)	
+					t.Logf("RAMUsageMax[Pass]: ~ %v", ramMaxUsageValues.PrometheusUsageMax)
+				}
+			}
+
+			ramUsageMaxNamespaceMap := make(map[string]*RAMUsageMaxAggregate)
+			for _, ramMaxUsageValues := range ramUsageMaxPodMap {
+				ramUsageNamespaceValue, ok := ramUsageMaxNamespaceMap[ramMaxUsageValues.Namespace]
+				if !ok {
+					ramUsageMaxNamespaceMap[ramMaxUsageValues.Namespace] = &RAMUsageMaxAggregate{
+						PrometheusUsageMax: ramMaxUsageValues.PrometheusUsageMax,
+						AllocationUsageMax: ramMaxUsageValues.AllocationUsageMax,
+						Namespace:          ramMaxUsageValues.Namespace, // Adds no value, Just for Clarity
+					}
+					continue
+				}
+				ramUsageNamespaceValue.AllocationUsageMax = max(ramUsageNamespaceValue.AllocationUsageMax, ramMaxUsageValues.AllocationUsageMax)
+				ramUsageNamespaceValue.PrometheusUsageMax = max(ramUsageNamespaceValue.PrometheusUsageMax, ramMaxUsageValues.PrometheusUsageMax)
+			}
+
+			t.Logf("\nMax Values for Namespaces.\n")
+			// Windows are not accurate for prometheus and allocation
+			for namespace, ramMaxUsageValues := range ramUsageMaxNamespaceMap {
+				t.Logf("Namespace %s", namespace)
+				if !AreWithinPercentage(ramMaxUsageValues.PrometheusUsageMax, ramMaxUsageValues.AllocationUsageMax, tolerance) {
+					t.Errorf("RAMUsageMax[Fail]: Prometheus: %0.2f, /allocation: %0.2f", ramMaxUsageValues.PrometheusUsageMax, ramMaxUsageValues.AllocationUsageMax)
+				} else {
+					t.Logf("RAMUsageMax[Pass]: ~ %v", ramMaxUsageValues.PrometheusUsageMax)
 				}
 			}
 		})
--- test/integration/prometheus/test.bats
+++ test/integration/prometheus/test.bats
@@ -10,10 +10,13 @@ teardown() {
     go test ./test/integration/prometheus/prometheus_test.go
 }
 
-@test "prometheus: RAM Costs" {
+@test "prometheus: RAMBytes, RAMBytesHours and RAMRequestAverage Costs" {
     go test ./test/integration/prometheus/consolidated_ram_costs_analysis_test.go
 }
 
+@test "prometheus: Max RAM Usage Costs" {
+    go test ./test/integration/prometheus/consolidated_ram_costs_analysis_test.go
+}
 
 @test "prometheus: PromQL URL Constructor Test" {
     go test ./test/integration/prometheus/client_test.go
