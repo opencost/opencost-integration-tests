6f68006c1b8196ff652c68ffccb6fd23614b2b76
Manas Sivakumar <manas23601@gmail.com>
2025-08-22 19:37:45 +0530

    fix promwindow resolution

--- test/integration/prometheus/ram_bytehours__ram_request_average_costs_analysis_test.go
+++ test/integration/prometheus/ram_bytehours__ram_request_average_costs_analysis_test.go
@@ -174,7 +174,7 @@ func TestRAMCosts(t *testing.T) {
 				promPodInfoInput.AggregateBy = []string{"container", "pod", "namespace", "node"}
 				promPodInfoInput.Function = []string{"avg"}
 				promPodInfoInput.AggregateWindow = tc.window
-				promPodInfoInput.AggregateResolution = "5m"
+				promPodInfoInput.AggregateResolution = "1m"
 				promPodInfoInput.Time = &endTime
 
 				podInfo, err := client.RunPromQLQuery(promPodInfoInput)
@@ -206,7 +206,7 @@ func TestRAMCosts(t *testing.T) {
 				// Create a map of PodData for each pod, and calculate the runtime.
 				// The query we make for pods is _NOT_ an average over time "range" vector. Instead, it's a range vector
 				// that returns _ALL_ samples for the pod over the last 24 hours. So, when you see <metric>[24h:5m] the
-				// number after the ':' (5m) is the resolution of the samples. So for the pod query, we get 24h / 5m
+				// number after the ':' (1m) is the resolution of the samples. So for the pod query, we get 24h / 1m
 				// ----------------------------------------------
 
 				// Pointers to modify in place
